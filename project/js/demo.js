e5a7333b6cde64a6dd22ff86e9a0befa09808f86
PriyankaJadhav10 <88777933+PriyankaJadhav10@users.noreply.github.com>
2021-11-26 13:47:42 +0530

    HTML AND CSS Using WEBSITE

---
+++ project/js/demo.js
@@ -0,0 +1,28 @@
+
+
+
+$(document).ready(function(){
+    $('#text').hide();
+
+$('#btn-search').click(function(e){
+    e.preventDefault();
+    $('#text').toggle();
+
+});
+
+
+
+$('nav').hide();
+
+
+
+$('#menu-bar').click(function(e){
+    e.preventDefault();
+    $('nav').toggle();
+
+});
+
+
+});
+
+
